49470d4367112a42489ff0465cea7ce1c7d44dd9
Nabanita Debnath <nitanabadev@gmail.com>
2025-03-18 11:53:46 +0530

    commit in dev

--- src/test/java/E2EFramework/TestRunner/TestNGTestRunner.java
+++ src/test/java/E2EFramework/TestRunner/TestNGTestRunner.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "Features/Employee.feature", glue="E2EFramework.stepDefinitions",
-monochrome = true,dryRun = true, plugin={"html:target/cucumber.html"})
+monochrome = true,dryRun = true, plugin={"html:target/cucumber.html","json:target/cucumber.json"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
+    //changed
 }

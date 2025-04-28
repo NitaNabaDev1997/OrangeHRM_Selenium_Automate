15970611f262acc8e02387dd6cecf5de92b5303f
NitaNabaDev1997 <nitanabadev@gmail.com>
2025-04-28 16:24:58 +0530

    updated

--- src/main/java/E2EFramework.pages/PIMPage.java
+++ src/main/java/E2EFramework.pages/PIMPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class PIMPage extends BasePage{
     public static WebDriver driver;
@@ -11,6 +12,7 @@ public class PIMPage extends BasePage{
     {
         super(driver);
         this.driver=driver;
+        PageFactory.initElements(driver,this);
     }
 
     @FindBy(xpath="//li[@class='oxd-main-menu-item-wrapper']//span[text()='PIM']")
--- src/test/java/E2EFramework/TestRunner/TestNGTestRunner.java
+++ src/test/java/E2EFramework/TestRunner/TestNGTestRunner.java
@@ -4,8 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = {".//Features/"}, glue="E2EFramework.stepDefinitions",
-monochrome = true,publish = true, tags = "@LoginEmployee",plugin={"html:target/cucumber.html","json:target/cucumber.json",
+@CucumberOptions(features = {".//Features/"}, //features= {"@target/rerun.txt"},
+ glue="E2EFramework.stepDefinitions",
+monochrome = true,publish = true, tags = "@LoginDataDrivenEmployee",plugin={"html:target/cucumber.html","json:target/cucumber.json",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
